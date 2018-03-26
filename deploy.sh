3a6dc20422a40f1d28459bf0a9831bcc2a0aa385
cheyuwu <frank890417@gmail.com>
2018-03-26 12:42:53 +0800

    add start sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 git push
-ssh pi@192.168.8.5  "cd mushroom;git pull;"
-ssh pi@192.168.8.7  "cd mushroom;git pull;"
-ssh pi@192.168.8.23  "cd mushroom;git pull;"
-ssh pi@192.168.8.24  "cd mushroom;git pull;"
+ssh pi@192.168.8.5  "cd mushroom;sudo git pull;"
+ssh pi@192.168.8.7  "cd mushroom;sudo git pull;"
+ssh pi@192.168.8.23  "cd mushroom;sudo git pull;"
+ssh pi@192.168.8.24  "cd mushroom;sudo git pull;"
