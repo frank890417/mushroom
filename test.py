aa781e5d78bb099b4e96360ca31abaa818a488f0
cheyuwu <frank890417@gmail.com>
2018-04-12 11:07:56 +0800

    update ip

--- test.py
+++ test.py
@@ -1,26 +1,20 @@
 
-#from pygame import mixer
 import time
-#import vlc
 import json
 import urllib.request
 import subprocess
-
+from subprocess import Popen
 import urllib
 
 while True:
-
     print("Check Audio")
-    url = "http://192.168.8.200/audiodeliver/get_music.php"
+    url = "http://140.114.40.21//audiodeliver/get_music.php"
     wp = urllib.request.urlopen(url)
     pw = wp.read()
-    #print(pw.decode("utf-8")
     data = json.loads(pw.decode("utf-8") )
+
     print(data[-1])
-
-    urllib.request.urlretrieve ("http://192.168.8.200/audiodeliver"+data[-1]['file'],"./audio/"+data[-1]['file'].split("/")[-1])
-
-    #print(data)
+    urllib.request.urlretrieve ("http://140.114.40.21//audiodeliver"+data[-1]['file'],"./audio/"+data[-1]['file'].split("/")[-1])
     subprocess.call(["omxplayer", "./audio/"+data[-1]['file'].split("/")[-1] ])
 
     time.sleep(6)
