8ccf8110c3bb99fc328b434e3a49aa24104a2137
cheyuwu <frank890417@gmail.com>
2018-04-19 11:11:57 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@ git push
 ssh pi@192.168.8.5  "cd mushroom;sudo git pull;"
 ssh pi@192.168.8.7  "cd mushroom;sudo git pull;"
 ssh pi@192.168.8.23  "cd mushroom;sudo git pull;"
-ssh pi@192.168.8.24  "cd mushroom;sudo git pull;"
+ssh pi@192.168.0.176  "cd mushroom;sudo git pull;"
--- test.py
+++ test.py
@@ -8,13 +8,14 @@ import urllib
 
 while True:
     print("Check Audio")
-    url = "http://140.114.40.21//audiodeliver/get_music.php"
+    domain = "https://techart.nthu.edu.tw/THE2018/"
+    url = domain+"audiodeliver/get_music.php"
     wp = urllib.request.urlopen(url)
     pw = wp.read()
     data = json.loads(pw.decode("utf-8") )
 
     print(data[-1])
-    urllib.request.urlretrieve ("http://140.114.40.21//audiodeliver"+data[-1]['file'],"./audio/"+data[-1]['file'].split("/")[-1])
+    urllib.request.urlretrieve (domain+"audiodeliver"+data[-1]['file'],"./audio/"+data[-1]['file'].split("/")[-1])
     subprocess.call(["omxplayer", "./audio/"+data[-1]['file'].split("/")[-1] ])
 
     time.sleep(6)
