05359debf6de0ee5263dbf9b6adffa79be1ea4c6
cheyuwu <frank890417@gmail.com>
2018-04-20 19:52:15 +0800

    update domain

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ import os.path
 
 while True:
     print("Check Audio")
-    domain = "https://awiclass.monoame.com/audiodeliver/"
+    domain = "https://awiclass.monoame.com/"
     url = domain+"audiodeliver/get_music.php"
     wp = urllib.request.urlopen(url)
     pw = wp.read()
